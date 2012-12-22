4b91cac9d49a5f83d7f258bfb39f3e75bdda0b0f
Richy501 <soulfear91@gmail.com>
2012-12-22 13:09:57 +0100

    Perfect jumping

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,15 +7,18 @@ int main() {
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
     int cont = 0;
-    bool derecha = false;
+    bool walk = false;
     float vely = 0;
     float velx = 0;
-    bool salto = false;
-    bool suelo = true;
+    bool jump = false;
+    bool ground = true;
+    bool endjump = false;
     sf::Texture sold;
+    sf::Text text;
+
     sold.setSmooth(true);
+
     // Load a sprite to display
-    //Asigno las imagenes a la clase texture
     if(!sold.loadFromFile("res/sprite/raptor.gif")) {
         return EXIT_FAILURE;
     }
@@ -24,13 +27,21 @@ int main() {
     if (!font.loadFromFile("res/font/arial.ttf")) {
         return EXIT_FAILURE;
     }
-    sf::Text text("U MAD BECAUSE I WIN, LOLOLOLO!", font, 50);
+    text.setString("U MAD BECAUSE I WIN, LOLOLOLO!");
+    text.setFont(font);
+    text.setCharacterSize(50);
     text.setPosition(400,300);
-    //Asigno la textura al sprite
-    //Y guardo la posicion
+
+    //I declare the vector of sprite
+    //when isnt moving (in the begining arent moving)
     sf::IntRect r1(0, 883, 34, 48);
-    sf::Sprite spr_sol(sold,r1);
+    //I declare the sprite with the vector and
+    //the texture
+    sf::Sprite spr_sol(sold, r1);
+
+    //I send where the soldier will begging
     spr_sol.setPosition(0, 400);
+
     // Load a music to play
     sf::Music music;
     if (!music.openFromFile("res/music/sonic.ogg")) {
@@ -39,36 +50,39 @@ int main() {
 
     // Play the music
     music.play();
+
+    //If I dont put it, in linux can running more fast
     window.setFramerateLimit(500);
+
     // Start the game loop
     while (window.isOpen()) {
         // Process events
         sf::Event event;
         while (window.pollEvent(event)) {
-            derecha = false;
-            salto = false;
+            //On the begining, the soldier no are walking
+            //and no are jumping
+            walk = false;
+            jump = false;
             // Close window : exit
             if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
                 window.close();
             }
 
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
                 //Jump
-                if(spr_sol.getPosition().y <= (spr_sol.getScale().y * -2)-400){
-                    salto = false;
-                }
-                else{
-                    suelo = false;
-                    salto = true;
-                    vely = -0.5;
+                ground = false;
+                if(!endjump) {
+                    jump = true;
+                    vely = -1;
                 }
             }
-            //Here start the other soldier
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-                derecha = true;
-                velx = 0.5;
+
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+                walk = true;
+                velx = -0.5;
                 r1.width = 41;
-                //spr_sol.move(vel,0);
+
+                //Creating the feeling of moving with sprites
                 if(r1.left == 40 * 2) {
                     r1.width = 43;
                 }
@@ -77,37 +91,68 @@ int main() {
                 if(r1.left > r1.width * 7) {
                     r1.left = 0;
                 }
-            } else {
-                r1.width = 34;
-                r1.top = 883;
+            }
+
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+                walk = true;
+                velx = 0.5;
+
+                //Creating the feeling of moving with sprites
+                r1.width = 41;
+                if(r1.left == 40 * 2) {
+                    r1.width = 43;
+                }
+                r1.top = 1;
                 r1.left += r1.width;
-                if(r1.left > r1.width * 2) {
+                if(r1.left > r1.width * 7) {
                     r1.left = 0;
                 }
             }
         }
 
-        if(!derecha) {
+        if(!walk) {
+            r1.width = 34;
+            r1.top = 883;
+            r1.left += r1.width;
+            if(r1.left > r1.width * 2) {
+                r1.left = 0;
+            }
             velx = 0;
         }
 
-        if(!salto && !suelo) {
-            vely = 0.5;
-            if(spr_sol.getPosition().y == 400){
-                suelo = true;
+        //The soldier cant jumping the double of its size
+        //when the jumping start in 400 Y
+        if(spr_sol.getPosition().y <= ((34 * -2) + 400) - 34) {
+            jump = false;
+            ground = false;
+            endjump = true;
+        }
+
+        if((!jump && !ground) || endjump) {
+            vely = 1;
+            endjump = true;
+            if(spr_sol.getPosition().y == 400) {
+                ground = true;
+                endjump = false;
                 vely = 0;
             }
         }
+
+        //The text will rotate allways
         text.setRotation(cont);
         text.setOrigin(400,25);
         cont++;
+
         spr_sol.move(velx,vely);
+
         // Clear screen
         window.clear();
         window.draw(text);
+
         // Draw the sprite
         spr_sol.setTextureRect(r1);
         window.draw(spr_sol);
+
         // Update the window
         window.display();
     }
